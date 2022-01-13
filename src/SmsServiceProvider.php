671a490883d2c3f9e977ddc736b317d05fdb194c
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2022-01-13 22:22:23 +0500

    sms service implemented

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PlayMobile\SMS;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+        
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/config.php' => config_path('playmobile.php')
+            ], 'playmobile-config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/config/config.php', 'playmobile');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('playmobile', function () {
+            return new SmsService;
+        });
+    }
+}
