443a67882385e4df98862b68a6334e6ff8d468c0
AzizbekDev <36416819+AzizbekDev@users.noreply.github.com>
2022-01-14 12:00:01 +0500

    migration file added

---
+++ src/database/migrations/2022_01_13_200456_create_sms_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSmsLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sms_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone', 9)->nullable();
+            $table->string('message', 191)->nullable();
+            $table->text('response')->nullable();
+            $table->tinyInteger('status',)->default(1)->comment('0-faild, 1-new, 2-success, 3-test');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sms_logs');
+    }
+}
